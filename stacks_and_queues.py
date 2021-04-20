e80f666367fb98927c6b7807288a457426634191
Hunter Laine <hunterglaine@gmail.com>
2021-04-19 20:15:02 -0700

    Question 2 on stacks and queues

---
+++ stacks_and_queues.py
@@ -0,0 +1,33 @@
+### 3.1 Three in One 
+
+
+### 3.2 Stack Min
+# Design a stack in which in addition to pop() and push() it also has a min()
+# method that returns the minimum value of the stack in constant time
+
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.min = None
+
+
+    def push(self, new_value):
+
+        self.stack.append(new_value)
+        if not self.min:
+            self.min = new_value
+        elif self.min > new_value:
+            self.min = new_value
+
+    
+    def pop(self):
+
+        if self.stack:
+            gone = self.stack.pop()
+            # if gone == self.min:
+
+        
+    
+    def min(self):
+
+        return self.min
