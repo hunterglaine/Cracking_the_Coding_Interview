f9f8e23344eec233279d41a763e4846e02f6dac7
Hunter Laine <hunterglaine@gmail.com>
2021-04-22 22:17:01 -0700

    Finish Question 3.3 Stack Of Stacks

--- stacks_and_queues.py
+++ stacks_and_queues.py
@@ -32,3 +32,31 @@ class MinStack:
     def min(self):
 
         return self.min
+
+
+### 3.3 Stack of Plates
+# Implement a data structure, StackOfStacks, composed of several stacks that creates a new stack when
+# the previous stack exceeds capacity. It should have a push() and pop() method that returns the same
+# type of values as a singular stack would
+
+class StackOfStacks:
+    def __init__(self, capacity):
+        self.stacks = []
+        self.capacity = capacity
+
+    def push(self, value):
+        if self.stacks:
+            if len(self.stacks[-1]) < self.capacity:
+                self.stacks[-1].append(value)
+            else:
+               self.stacks.append([value]) 
+        else:
+            self.stacks.append([value])
+
+    def pop(self):
+        if self.stacks:
+            popped = self.stacks[-1].pop()
+            if not self.stacks[-1]:
+                self.stacks.pop()
+            return popped
+
